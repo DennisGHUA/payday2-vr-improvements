ce2bf7d53d71fc310261e44d39f2c626c4f42e26
Campbell Suter <znix@znix.xyz>
2017-11-20 21:43:03 +1300

    Fix crash-on-startup caused by redout when FPCameraPlayerBase isn't loaded

--- menumanagervr.lua
+++ menumanagervr.lua
@@ -6,10 +6,12 @@
 
 -- When the user opens the menu, hide the redout effect
 Hooks:PostHook(MenuManagerVR, "_enter_menu_room", "VRPlusOpenMenuHideRedout", function()
-	local r = FPCameraPlayerBase.__redout
-	if r then
-		r.effect.color.alpha = 0
-	end
+	if
+		not FPCameraPlayerBase
+		or not FPCameraPlayerBase.__redout
+	then return end
+
+	FPCameraPlayerBase.__redout.effect.color.alpha = 0
 end)
 
 -- Don't need to reapply it - it's computed each tick in FPCameraPlayerBase
