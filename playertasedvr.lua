228b6c5e7e6d3088b164219b1d2ea2c72e8fc140
Campbell Suter <znix@znix.xyz>
2018-01-16 10:33:52 +1300

    Remove taser crash fix, now in base game

--- playertasedvr.lua
+++
@@ -1,9 +0,0 @@
---[[
-	PlayerTasedVR
-
-	Prevent crashes when tased - See #37
---]]
-
-Hooks:PostHook(PlayerTasedVR, "init", "VRPlusFixTaserCrash", function(self)
-	self._shooting_weapons = {}
-end)
