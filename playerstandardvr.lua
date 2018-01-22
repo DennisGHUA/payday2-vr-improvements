08b779b07c3a6f76802dedf12f5c34f3b8cf3471
Campbell Suter <znix@znix.xyz>
2018-01-22 14:03:16 +1300

    Delete outdated copied PlayerStandard:_get_max_walk_speed hook, Fixes #90

--- playerstandardvr.lua
+++ playerstandardvr.lua
@@ -8,64 +8,10 @@ dofile(ModPath .. "playerstandardvr/warpidlestate.lua")
 dofile(ModPath .. "playerstandardvr/warptargetstate.lua")
 dofile(ModPath .. "playerstandardvr/ladders.lua")
 
--- The VR implementation of this doesn't take sprinting into
--- account - therefore the player moves at sprinting speed nomatter
--- what. Fixes #1
--- TODO find some way to avoid overridding the whole method
+-- Apply the movement speed cap
 local old_get_max_walk_speed = PlayerStandard._get_max_walk_speed
-function PlayerStandard:_get_max_walk_speed(t)
-	if not VRPlusMod._data.movement_locomotion then
-		return old_get_max_walk_speed(self, t)
-	end
-
-	-- Note: this is identical to the non-vr version of this function
-	local speed_tweak = self._tweak_data.movement.speed
-	local movement_speed = speed_tweak.STANDARD_MAX
-	local speed_state = "walk"
-
-	if self._state_data.in_steelsight and not managers.player:has_category_upgrade("player", "steelsight_normal_movement_speed") then
-			movement_speed = speed_tweak.STEELSIGHT_MAX
-			speed_state = "steelsight"
-	elseif self:on_ladder() then
-			movement_speed = speed_tweak.CLIMBING_MAX
-			speed_state = "climb"
-	elseif self._state_data.ducking then
-			movement_speed = speed_tweak.CROUCHING_MAX
-			speed_state = "crouch"
-	elseif self._state_data.in_air then
-			movement_speed = speed_tweak.INAIR_MAX
-			speed_state = nil
-	elseif self._running then
-			movement_speed = speed_tweak.RUNNING_MAX
-			speed_state = "run"
-	end
-
-	movement_speed = managers.crime_spree:modify_value("PlayerStandard:GetMaxWalkSpeed", movement_speed, self._state_data, speed_tweak)
-	local morale_boost_bonus = self._ext_movement:morale_boost()
-	local multiplier = managers.player:movement_speed_multiplier(speed_state, speed_state and morale_boost_bonus and morale_boost_bonus.move_speed_bonus, nil, self._ext_damage:health_ratio())
-	local apply_weapon_penalty = true
-
-	if self:_is_meleeing() then
-			local melee_entry = managers.blackmarket:equipped_melee_weapon()
-			apply_weapon_penalty = not tweak_data.blackmarket.melee_weapons[melee_entry].stats.remove_weapon_movement_penalty
-	end
-
-	if alive(self._equipped_unit) and apply_weapon_penalty then
-			multiplier = multiplier * self._equipped_unit:base():movement_penalty()
-	end
-
-	if managers.player:has_activate_temporary_upgrade("temporary", "increased_movement_speed") then
-			multiplier = multiplier * managers.player:temporary_upgrade_value("temporary", "increased_movement_speed", 1)
-	end
-
-	local final_speed = movement_speed * multiplier
-	self._cached_final_speed = self._cached_final_speed or 0
-
-	if final_speed ~= self._cached_final_speed then
-		self._cached_final_speed = final_speed
-
-		self._ext_network:send("action_change_speed", final_speed)
-	end
+function PlayerStandard:_get_max_walk_speed(...)
+	local final_speed = old_get_max_walk_speed(self, ...)
 
 	-- Apply a speed cap, as per the comfort options
 	if VRPlusMod._data.comfort.max_movement_speed_enable then
