57d54b8d936d7544fe73a314362e280380e130b2
Campbell Suter <znix@znix.xyz>
2018-08-01 19:25:57 +1200

    Apply changes made in the controls customisation menu to the game input mappings (ie, make them functional) - Implements #71

--- handstatesplayer.lua
+++ handstatesplayer.lua
@@ -1,3 +1,21 @@
+--[[
+	Hand States Player
+
+	Apply the custom control scheme as set by the player.
+
+	This also applies the older fixed-style customisations, which I'd
+	like to find an elegant replacement for, as tick-box customisation is
+	quicker/easier for the user than mapping the controls across several differnt
+	states.
+]]
+
+-- The fixed customisations
+-- Register these first so user-defined customisations override
+--  these, not vise-versa.
+-- Note these apply to the defaults (but NOT in the UI), so if the user resets
+--  the customisation for a control it will still include these.
+-- TODO make these appear in the defaults in some way
+-- TODO add some kind of note in the UI to indicate such changes.
 
 local function add_offhand_actions(hand_name, key_map)
 	if VRPlusMod._data.comfort.crouching ~= VRPlusMod.C.CROUCH_NONE then
@@ -122,3 +140,59 @@ local states = {
 VRPlusMod._ControlManager._handstatesplayerdata = {
 	states = states
 }
+
+for _, state in ipairs(states) do
+	local class = _G[state .. "HandState"]
+
+	Hooks:PostHook(class, "apply", "VRPlusCustomInputs_" .. state, function(self, hand, key_map)
+		local hand_name = hand == 1 and "r" or "l"
+
+		-- Grab the customisations
+		-- The format of the table is as follows:
+		--[[
+{
+	menu = { -- The ID of the button, minus the _l or _r suffix indicating which
+	         --  hand it's on
+		reload = {}, -- The action to be taken, and what special settings it has
+		toggle_menu = { hand = 2 }, -- Only apply this input to the left hand
+	}
+}
+		--]]
+		local rebindings = VRPlusMod._data.control_rebindings[state]
+		if not rebindings then return end
+
+		for input_id, actions in pairs(rebindings) do
+			-- The list of actions to be taken (running with out previous example,
+			--  this would be {"reload"} or {"reload", "toggle_menu"} depending on
+			--  which hand this is being bound to
+			local result_action_list = {}
+
+			-- The input ID as used by the game, eg menu_l or menu_r
+			local handed_input_id = input_id .. "_" .. hand_name
+
+			for action, action_data in pairs(actions) do
+				local actinfo = VRPlusMod._ControlManager.Data.actions[action]
+				local action_id
+
+				if actinfo.right then
+					-- If there is a special set of action IDs depending on
+					--  the hand, match for those
+					action_id = hand == 1 and actinfo.right or actinfo.left
+				else
+					action_id = action
+				end
+
+				-- If the action is not limited to one particular hand, or if
+				--  it's limited to the hand we're currently setting up, add
+				--  it to the list of actions to take when this input is used
+				if not action_data.hand or action_data.hand == hand then
+					table.insert(result_action_list, action_id)
+				end
+			end
+
+			-- Set the resulting items to nil if we don't have any actions
+			--  bound to this input, otherwise set it to the list we just compiled
+			key_map[handed_input_id] = #result_action_list == 0 and nil or result_action_list
+		end
+	end)
+end
